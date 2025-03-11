3057fe7e82078370eaeaf9c311c30fe47f3d83be
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2025-03-11 14:47:41 +0600

    Added many things

--- src/interface/course-interface.ts
+++ src/interface/course-interface.ts
@@ -1,17 +1,22 @@
 import { Model, Types } from "mongoose";
+import { ICategory } from "./category-interface";
+import { IUser } from "./user-interface";
+import { ITestimonial } from "./tesimonial-interface";
+import { IModule } from "./module-interface";
 
 // Define the interface for the Course document
 export interface ICourse {
+  _id: Types.ObjectId; // Unique identifier for the course
   title: string;
   description: string;
   thumbnail: string;
-  modules: Types.ObjectId[]; // Reference to Module model
+  modules: IModule[]; // Reference to Module model
   price: number;
   active: boolean;
-  category: Types.ObjectId; // Reference to Category model
-  instructor: Types.ObjectId; // Reference to User model
+  category: ICategory; // Reference to Category model
+  instructor: IUser; // Reference to User model
   quizzes?: Types.ObjectId; // Optional, reference to Quiz model
-  testimonials: Types.ObjectId[]; // Array of references to Testimonial model
+  testimonials: ITestimonial[]; // Array of references to Testimonial model
 }
 
 export type CourseModel = Model<ICourse, Record<string, unknown>>;
