911c067b7470d415375ae1579d95557cd78c0604
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2025-03-24 14:03:52 +0600

    params updated

--- src/app/register/[role]/page.jsx
+++ src/app/register/[role]/page.jsx
@@ -1,6 +1,7 @@
 import { SignupForm } from "../_components/SignupForm";
 
-const RegisterPage = ({ params: { role } }) => {
+const RegisterPage = async ({ params }) => {
+  const { role } = await params;
   return (
     <div className="w-full flex-col h-screen flex items-center justify-center">
       <div className="container">
